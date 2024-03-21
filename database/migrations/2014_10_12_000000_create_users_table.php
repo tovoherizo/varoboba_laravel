2049812725f5afa38774f25839d4401ce2e71256
VAHATRINIAINA Tovoherizo OBS/GDO <gdgl6487@orange.com>
2024-03-21 21:31:36 +0100

    addic

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->char('sexe');
+
             $table->rememberToken();
             $table->timestamps();
         });
